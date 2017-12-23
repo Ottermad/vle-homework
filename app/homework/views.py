5c56635c8625eacc964718e2ae1992efaf5617b2
Charles Thomas <charlie.thomas@attwoodthomas.net>
2017-12-23 22:47:13 +0000

    Add submissions for homework listing

--- app/homework/views.py
+++ app/homework/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, g, jsonify
-from . models import Submission
+from . models import Submission, Homework
 from internal.decorators import permissions_required
+from internal.exceptions import UnauthorizedError
+from internal.helper import get_record_by_id
 
 homework_blueprint = Blueprint('homework', __name__, url_prefix='/homework')
 
@@ -11,3 +13,13 @@ def list_my_submissions():
     submissions = Submission.query.filter_by(user_id=g.user.id)
     submissions_list = [s.to_dict(nest_homework=True, nest_comments=True) for s in submissions]
     return jsonify({'success': True, 'submissions': submissions_list})
+
+
+@homework_blueprint.route('/homework/<int:homework_id>/submissions')
+@permissions_required({'Teacher'})
+def list_submissions(homework_id):
+    homework = get_record_by_id(homework_id, Homework, check_school_id=False)
+    if homework.lesson.school_id != g.user.school_id:
+        raise UnauthorizedError()
+    submissions = Submission.query.filter_by(homework_id=homework.id).all()
+    return jsonify({'success': True, 'submissions': [s.to_dict(nest_user=True) for s in submissions]})
