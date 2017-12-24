37d73fecd6eb22b96a57e4016307ae877c83f123
Charles Thomas <charlie.thomas@attwoodthomas.net>
2017-12-24 00:06:59 +0000

    Add quiz detail and submit

--- app/quiz/quiz_functions.py
+++ app/quiz/quiz_functions.py
@@ -6,7 +6,7 @@ from internal.helper import json_from_request, check_keys,get_record_by_id
 from internal.exceptions import UnauthorizedError,  CustomError
 
 from app import services, db
-from .models import Quiz, Question
+from .models import Quiz, Question, QuizAnswer, QuizSubmission
 
 
 def create_quiz(request):
@@ -67,3 +67,71 @@ def create_quiz(request):
     db.session.add(quiz)
     db.session.commit()
     return jsonify(quiz.to_dict()), 201
+
+
+def submit_quiz(request, quiz_id):
+    # Check quiz is valid
+    quiz = get_record_by_id(quiz_id, Quiz, check_school_id=False)
+    if quiz.lesson.school_id != g.user.school_id:
+        raise UnauthorizedError()
+
+    json_data = json_from_request(request)
+    expected_top_keys = ['answers']
+    expected_inner_keys = ['question_id', 'answer']
+
+    check_keys(expected_top_keys, json_data)
+
+    #  Validate lesson
+    resp = services.lesson.get(
+        "lessons/lesson/{}".format(json_data['lesson_id']), 
+        headers=g.user.headers_dict(),
+        params={'nest-students': True}
+    )
+    if resp.status_code != 200:
+        raise CustomError(**resp.json())
+
+    lesson = resp.json()['lesson']
+
+    if g.user.id not in [t['id'] for t in lesson['students']]:
+        raise UnauthorizedError()
+
+    question_ids = [question.id for question in quiz.questions]
+
+    submission = QuizSubmission(
+        homework_id=quiz.id,
+        user_id=g.user.id,
+        datetime_submitted=datetime.datetime.now()  # TODO: Deal with timezones
+    )
+
+    for answer in json_data['answers']:
+        check_keys(expected_inner_keys, answer)
+        question = get_record_by_id(answer['question_id'], Question, check_school_id=False)
+        if question.id not in question_ids:
+            raise UnauthorizedError()
+
+        answer = QuizAnswer(answer['answer'], submission.id, question.id)
+        submission.answers.append(answer)
+
+    submission.mark()
+    db.session.add(submission)
+    db.session.commit()
+    return jsonify({'score': submission.total_score})
+
+
+def quiz_detail(request, quiz_id):
+    # Check quiz if valid
+    quiz = get_record_by_id(quiz_id, Quiz, check_school_id=False)
+
+    resp = services.lesson.get(
+        "lessons/lesson/{}".format(quiz.lesson_id), 
+        headers=g.user.headers_dict(),
+    )
+    if resp.status_code != 200:
+        raise CustomError(**resp.json())
+
+    lesson = resp.json()['lesson']
+
+    if lesson['school_id'] != g.user.school_id:
+        raise UnauthorizedError()
+
+    return jsonify({'success': True, 'quiz': quiz.to_dict()})
--- app/quiz/views.py
+++ app/quiz/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, g
-from .quiz_functions import create_quiz
+from .quiz_functions import create_quiz, submit_quiz, quiz_detail
+from internal.decorators import permissions_required
 
 quiz_blueprint = Blueprint('quiz', __name__, url_prefix='/quiz')
 
@@ -9,3 +10,11 @@ def quiz_get_or_create():
     if request.method == 'POST':
         if g.user.has_permissions({'Teacher'}):
             return create_quiz(request)
+
+
+@quiz_blueprint.route('/<int:quiz_id>', methods=("POST", "GET"))
+@permissions_required({'Student'})
+def quiz_submit(quiz_id):
+    if request.method == "POST":
+        return submit_quiz(request, quiz_id)
+    return quiz_detail(request, quiz_id)
